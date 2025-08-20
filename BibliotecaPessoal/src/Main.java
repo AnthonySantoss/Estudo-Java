7e8a4918c4948725f5e0b6f3c99ba5db3f02c384
AnthonySantoss <16034688@sempreuninassau.com.br>
2025-08-20 09:53:09 -0300

    Criando a inicialização do sistema

--- BibliotecaPessoal/src/Main.java
+++ BibliotecaPessoal/src/Main.java
@@ -1,10 +1,33 @@
 import biblioteca.Biblioteca;
 import biblioteca.Livro;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int opcao;
+
+        do {
+            System.out.println("Biblioteca Pessoal");
+            System.out.println("-------------------");
+            System.out.println("Menu");
+            System.out.println("1 - Adicionar Livro");
+            System.out.println("2 - Listar Livros");
+            System.out.println("3 - Buscar por Título");
+            System.out.println("4 - Buscar por Autor");
+            System.out.println("0 - Fechar Biblioteca");
+
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+
+        } while (opcao != 0);
+
 
 
     }
